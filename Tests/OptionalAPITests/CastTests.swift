0814019b84236a2d817b31ad05df9440baa44d7d
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-06-18 23:46:40 +0200

    Adds tests for `cast` operator.

--- Tests/OptionalAPITests/CastTests.swift
+++ Tests/OptionalAPITests/CastTests.swift
@@ -3,59 +3,77 @@ import OptionalAPI
 
 
 class CastTests: XCTestCase {
-
+    
     func test_cast_should_returnCastedOptional() {
         // Arrange
         var didCast = false
         
         // Act
-        anyString
-            .cast(String.self)
-            .andThen ({ _ in
-                didCast = true
-            })
+        let result: String? = anyString.cast(String.self)
         
         // Assert
+        result
+            .mapNone({ () -> String in
+                XCTFail("Should be able to cast!")
+                return "fail"
+                }())
+            .andThen ({ (string: String) -> String in
+                didCast = true
+                
+                return string
+            })
+        
         XCTAssertTrue(
             didCast,
             "Should cast \(String(describing: anyString)) to String"
         )
-    }
-    
-    func test_cast_should_returnNone_whenCantCast() {
-        // Arrange
-        var didNotCast = false
-        
-        // Act
-        anyInt
-            .cast(String.self)
-            .mapNone({
-                didNotCast = true
-                return "default"
-                }())
-        
-        // Assert
-        XCTAssertTrue(
-            didNotCast,
-            "Should Not cast \(String(describing: anyInt)) to String"
+        XCTAssertEqual(
+            result, "any string"
         )
     }
     
-    func test_cast_shouldNot_castNoneCases() {
-        // Arrange
-        let didCast = expectation(description: "should not cast")
-        didCast.isInverted = true
+    func test_cast_shouldReturn_NoneForFailedCast() {
+        // Arrange && Act
+        let result: Int? = anyString.cast(Int.self)
         
-        defer { waitForExpectations(timeout: 0.005) }
-        
-        // Act
-        anyString
-            .cast(String.self)
-            
-            // Assert
-            .andThen ({ (castedString) in
-                XCTAssertEqual(castedString, anyString! as! String)
-                didCast.fulfill()
+        // Assert
+        result
+            .andThen ({ (number: Int) -> Int in
+                XCTFail("Should NOT cast!")
+                
+                return number
             })
+        
+        XCTAssertNil(result)
+    }
+    
+    func test_cast_toDifferentType_shouldReturn_NoneForNone() {
+        // Arrange && Act
+        let result: Int? = anyNoneString.cast(Int.self)
+        
+        // Assert
+        result
+            .andThen ({ (number: Int) -> Int in
+                XCTFail("Should NOT cast!")
+                
+                return number
+            })
+        
+        XCTAssertNil(result)
+    }
+    
+    func test_cast_toSameType_shouldReturn_NoneForNone() {
+        // Arrange && Act
+        let result: String? = anyNoneString.cast(String.self)
+        
+        // Assert
+        result
+            .andThen ({ (string: String) -> String in
+                XCTFail("Should NOT cast!")
+                
+                return string
+            })
+        
+        XCTAssertNil(result)
     }
 }
