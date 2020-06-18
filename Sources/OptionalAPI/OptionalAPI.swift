5abbbd4b94156b0fbe1bd68900b872255fed2f93
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-06-18 23:47:38 +0200

    Foxes a typo.

--- Sources/OptionalAPI/OptionalAPI.swift
+++ Sources/OptionalAPI/OptionalAPI.swift
@@ -4,7 +4,7 @@ import Foundation
 public extension Optional {
     
     /**
-     Typealias for a closure capable of producing an instance od **Wrapped** type.
+     Type alias for a closure capable of producing an instance od **Wrapped** type.
      */
     typealias ProducerOfWrapped = () -> Wrapped
     
