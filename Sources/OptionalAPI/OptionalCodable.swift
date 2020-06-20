755ab372e0cf120797506b9cfa0ed5b037b7632e
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-06-20 15:38:06 +0200

    Makes internal methods public.

--- Sources/OptionalAPI/OptionalCodable.swift
+++ Sources/OptionalAPI/OptionalCodable.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // MARK: - Extension
-extension Optional where Wrapped == Data {
+public extension Optional where Wrapped == Data {
     func decode<T: Decodable>(_ to: T.Type = T.self) -> T? {
         flatMap { (wrapped) -> T? in
             try? JSONDecoder().decode(T.self, from: wrapped)
@@ -9,10 +9,36 @@ extension Optional where Wrapped == Data {
     }
 }
 
-extension Optional where Wrapped: Encodable {
+public extension Optional where Wrapped: Encodable {
     func encode() -> Data? {
         flatMap { (wrapped) -> Data? in
             try? JSONEncoder().encode(wrapped)
         }
     }
+}
+
+// MARK: - Free Functions
+// MARK: Decode
+public func decode<T: Decodable>(
+    _ optional: Data?,
+    _ to: T.Type = T.self)
+    -> T?
+{
+    optional.decode(to)
+}
+
+public func decode<T: Decodable>(
+    _ to: T.Type = T.self)
+    -> (Data?) -> T?
+{
+    return { (optional: Data?) -> T? in
+        
+        optional.decode(to)
+    }
+}
+
+
+// MARK: Encode
+public func encode<T: Encodable>(_ optional: T?) -> Data? {
+    optional.encode()
 }
