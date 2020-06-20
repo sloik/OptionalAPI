0d8c2dee5248622edf01002f3823e9add8c75d22
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-06-20 15:37:43 +0200

    Adds tests for free unctions for codable methods.

--- Tests/OptionalAPITests/OptionalCodableTests.swift
+++ Tests/OptionalAPITests/OptionalCodableTests.swift
@@ -2,21 +2,24 @@ import XCTest
 @testable import OptionalAPI
 
 let codableStruct: CodableStruct = .init(number: 69, message: "codable message")
-let codableStructAsData: Data =
+
+let sut     : CodableStruct? = codableStruct
+let noneCase: CodableStruct? = .none
+
+let codableStructAsData: Data? =
     """
     {
         "number": 55,
         "message": "data message"
     }
     """.data(using: .utf8)!
+let noneData: Data? = .none
+
 
 final class OptionalCodableTests: XCTestCase {
 
     func test_shouldEncode() {
-        // Arrange
-        let sut: CodableStruct? = codableStruct
-        
-        // Act
+        // Arrange & Act
         let result = sut.encode()
         
         // Assert
@@ -75,5 +78,61 @@ final class OptionalCodableTests: XCTestCase {
         
         // Assert
         XCTAssertNil(result)
+    }
+    
+    func test_freeFunctions_should_yieldSameResultsAsExtension() {
+        // MARK: Encode
+        XCTAssertEqual(
+            sut.encode()!,
+            encode(sut)!
+        )
+        
+        XCTAssertEqual(
+            noneCase.encode(),
+            encode(noneCase)
+        )
+        
+        // MARK: Decode
+        XCTAssertEqual(
+            codableStructAsData.decode(CodableStruct.self),
+            decode(codableStructAsData, CodableStruct.self)
+        )
+        XCTAssertEqual(
+            codableStructAsData.decode(CodableStruct.self),
+            decode(CodableStruct.self)(codableStructAsData) // curried
+        )
+        
+        XCTAssertEqual(
+            noneData.decode(CodableStruct.self),
+            decode(noneData, CodableStruct.self)
+        )
+        XCTAssertEqual(
+            noneData.decode(CodableStruct.self),
+            decode(CodableStruct.self)(noneData) // curried
+        )
+    }
+    
+    func test_encodeDecode_randomStuff() {
+        XCTAssertEqual(
+            sut,
+            sut
+                .encode()
+                .decode(CodableStruct.self)
+                .encode()
+                .decode(CodableStruct.self)
+                .encode()
+                .decode(CodableStruct.self),
+            "Should not loose any information!"
+        )
+        
+        XCTAssertEqual(
+            CodableStruct(number: 55, message: "data message"),
+            codableStructAsData
+                .decode(CodableStruct.self)
+                .encode()
+                .decode(CodableStruct.self)
+                .encode()
+                .decode(CodableStruct.self)
+        )
     }
 }
