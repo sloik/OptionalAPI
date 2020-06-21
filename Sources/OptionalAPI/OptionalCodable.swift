857e85f6af34c24c963d7d5f68f44c83354e0ee1
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-06-21 15:37:52 +0200

    Adds documentation to extension methods.

--- Sources/OptionalAPI/OptionalCodable.swift
+++ Sources/OptionalAPI/OptionalCodable.swift
@@ -2,6 +2,30 @@ import Foundation
 
 // MARK: - Extension
 public extension Optional where Wrapped == Data {
+    
+    
+    /// Decodes wrapped data to desired Decodable type.
+    /// - Parameter to: Type that is conforming to `Decodable`.
+    /// - Returns: Instance of `T` if JSONDecoder decode succeeded or .none otherwise.
+    ///
+    /// ```
+    /// let codableStructAsData: Data? = ...
+    ///
+    /// let result: CodableStruct? = codableStructAsData.decode()
+    /// ```
+    /// Type can be inferred so it does not have to be added explicitly. Same can
+    /// be written as:
+    /// ```
+    /// let result = codableStructAsData.decode(CodableStruct.self)
+    /// ```
+    /// Either way is fine and you will have an optional to work with.
+    /// ```
+    /// codableStructAsData
+    ///     .decode(CodableStruct.self)
+    ///     .andThen({ instance in
+    ///         // work with not optional instance
+    ///     })
+    /// ```
     func decode<T: Decodable>(_ to: T.Type = T.self) -> T? {
         flatMap { (wrapped) -> T? in
             try? JSONDecoder().decode(T.self, from: wrapped)
@@ -10,6 +34,10 @@ public extension Optional where Wrapped == Data {
 }
 
 public extension Optional where Wrapped: Encodable {
+    
+    
+    /// Encodes wrapped value to Data.
+    /// - Returns: Data if JSONEncoder encode succeeded or .none otherwise.
     func encode() -> Data? {
         flatMap { (wrapped) -> Data? in
             try? JSONEncoder().encode(wrapped)
