0d3e45609dac3134dff6db3639e61965db7377e2
Ed Maliniak <>
2021-06-26 21:22:56 +0200

    Adds comments to have a way to quickly see values in code.

--- Tests/OptionalAPITests/SUTs.swift
+++ Tests/OptionalAPITests/SUTs.swift
@@ -1,19 +1,35 @@
 import Foundation
 
 // MARK: - Systems Under Test
-let noneString     : String? = .none
 
+/// `.none`
+let noneString: String? = .none
+
+/// ""
 let emptySomeString: String? = ""
-let someSomeString : String? = "some string"
 
+/// "some string"
+let someSomeString: String? = "some string"
+
+/// `.none`
 let noneIntArray : [Int]? = .none
+
+/// Empty array `[]`
 let emptyIntArray: [Int]? = []
+
+/// Array with elements `[11, 22, 33]`.
 let someIntArray : [Int]? = [11, 22, 33]
 
+/// .none
 let noneInt: Int? = .none
+
+/// `.some( 42 )`
 let someInt: Int? = .some(42)
 
+/// `String?.some("any string")`
 let anyString: Any? = String?.some("any string")
+
+/// `String?.none`
 let anyNoneString: Any? = String?.none
 
 let anyInt: Any? = someInt
