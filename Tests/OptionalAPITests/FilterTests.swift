8fedfea71acbea7e0ce82fda94babeebb9bbcfbc
Lukasz Stocki <stocki.lukasz@gmail.com>
2022-10-28 22:53:36 +0200

    Adds tests for filter.

--- Tests/OptionalAPITests/FilterTests.swift
+++ Tests/OptionalAPITests/FilterTests.swift
@@ -4,27 +4,44 @@ import OptionalAPI
 
 class FilterTests: XCTestCase {
 
+    let sutTrue: (Int?) -> Int? = OptionalAPI.filter( alwaysTruePredicate )
+    let sutFalse: (Int?) -> Int? = OptionalAPI.filter( alwaysFalsePredicate )
+
     func test_filteringSomeOptional_withSuccessPredicate_shouldBeSome() {
         XCTAssertNotNil(
-            Int?.some(42).filter(alwaysTruePredicate)
+            sutTrue( 42 )
         )
     }
     
     func test_filteringNoneOptional_withSuccessPredicate_shouldBeNone() {
         XCTAssertNil(
-            Int?.none.filter(alwaysTruePredicate)
+            sutTrue( .none )
         )
     }
     
     func test_filteringSomeOptional_withFailurePredicate_shouldBeNone() {
         XCTAssertNil(
-            Int?.some(42).filter(alwaysFalsePredicate)
+            sutFalse( 42 )
         )
     }
     
     func test_filteringNoneOptional_withFailurePredicate_shouldBeNone() {
         XCTAssertNil(
-            Int?.none.filter(alwaysFalsePredicate)
+            sutFalse( .none )
+        )
+    }
+
+    func test_api() {
+        let arrayWithTwoElements: [Int]? = [42, 69]
+
+        XCTAssertNotNil(
+            arrayWithTwoElements
+                .filter { array in array.count > 1 }
+        )
+
+        XCTAssertNil(
+            arrayWithTwoElements
+                .filter { array in array.isEmpty }
         )
     }
 }
